20854cd3b82edcd01fe731ccc4cc87156e0d336e
Bozhan Shang <143424738+bz421@users.noreply.github.com>
2026-06-16 07:33:39 -0400

    set cron to 7:35 (temp)

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -297,7 +297,7 @@ export const onMarketDigest = inngest.createFunction(
     {
         id: 'market-digest',
         triggers: [
-            { cron: 'TZ=America/New_York 0 7 * * *' },
+            { cron: 'TZ=America/New_York 35 7 * * *' },
             { cron: 'TZ=America/New_York 0 12 * * *' },
             { cron: 'TZ=America/New_York 0 18 * * *' },
         ]
